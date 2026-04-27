57b5583769000db1d1d26bffdc91cf4ddf7585c3
leehaeyeon123 <haeyeon592@gmail.com>
2026-04-27 13:49:42 +0900

    fix

--- src/main/java/com/nbang/WebConfig.java
+++ src/main/java/com/nbang/WebConfig.java
@@ -8,8 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // 모든 API에 대해
-                .allowedOrigins("https://n-b-bang-frontend-5g58.vercel.app") // 리액트 주소 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://n-b-bang-frontend-5g58.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
