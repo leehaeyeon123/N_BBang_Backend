26f823d91fb9017220e3a3b7da02d93fcc31911a
leehaeyeon123 <haeyeon592@gmail.com>
2026-04-27 14:03:40 +0900

    fix

--- src/main/java/com/nbang/WebConfig.java
+++ src/main/java/com/nbang/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://n-b-bang-frontend.vercel.app/")
+                .allowedOrigins("https://n-b-bang-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
